c123e96fad9ccbccc99f03953a82d3d8542950b7
nickbassirpour <nickbassirpour@gmail.com>
2024-03-02 14:01:42 -0800

    testing static linking

--- app/(tabs)/home/[category]/[url]/index.tsx
+++ app/(tabs)/home/[category]/[url]/index.tsx
@@ -1,22 +1,27 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { useLocalSearchParams } from "expo-router";
+import WebView from "react-native-webview";
 
 const Article = () => {
   const { category, url } = useLocalSearchParams();
 
-  const article = `./${category}/${url}.html`;
-
+  const article = `../../../../../data/articles/SOD/j197sd_FrancisAssisr_10-04.html`;
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        rowGap: 20,
       }}
     >
-      <Text>{`${article}`}</Text>
+      <WebView
+        originWhitelist={["*"]}
+        source={require(article)}
+        scrollEnabled={true}
+        style={{
+          flex: 1,
+          border: 10,
+        }}
+      />
     </View>
   );
 };
